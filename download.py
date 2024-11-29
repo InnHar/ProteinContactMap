f601663528c70ab02abcc31a4009a3ecd020d500
InnHar <innaharutyunyan944@gmail.com>
2024-11-29 21:34:55 +0400

    download with threading

--- download.py
+++ download.py
@@ -2,6 +2,7 @@ import requests
 import gzip
 import shutil
 import os
+from concurrent.futures import ThreadPoolExecutor
 
 # Path to the file
 file_path = r"C:\Users\ASUS\PycharmProjects\ProteinContactMap\links.txt"
@@ -10,7 +11,9 @@ file_path = r"C:\Users\ASUS\PycharmProjects\ProteinContactMap\links.txt"
 with open(file_path, 'r') as file:
     content = file.read()
 
-for file_path in content.split('\n')[:5]:
+
+# Function to process a single file
+def process_file(file_path):
     url = f"https://files.rcsb.org/pub/pdb/data/structures/all/pdb/{file_path}"
     save_dir = 'Training set'
 
@@ -33,11 +36,21 @@ for file_path in content.split('\n')[:5]:
             with open(decompressed_file_path, 'wb') as f_out:
                 shutil.copyfileobj(f_in, f_out)
         os.remove(gz_file_path)
+
         t = False
         with open(decompressed_file_path, 'rb') as f_in:
             content = f_in.read().decode('utf-8')
             if 'DNA' in content[:20] or 'RNA' in content[:20]:
                 t = True
+
         if t:
             os.remove(decompressed_file_path)
+
         print(f"Decompressed file saved as '{decompressed_file_path}'.")
+
+
+# Use ThreadPoolExecutor to process files concurrently
+with ThreadPoolExecutor() as executor:
+    # Split the file paths from the content and submit tasks to the executor
+    file_paths = content.split('\n')[:100]  # Just processing the first 5 files, you can adjust this
+    executor.map(process_file, file_paths)
